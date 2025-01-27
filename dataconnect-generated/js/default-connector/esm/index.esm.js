565e6d868c472fea7ad53e5f376347957c690957
juanvolpe <juan.volpe90@gmail.com>
2025-01-27 00:33:48 -0500

    Remove all remaining Firebase files and configurations

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { , validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'productivityappmzcopy2copy',
-  location: 'us-central1'
-};
-
