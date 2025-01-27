82c04b2fbb347790c16b5c46c50335e72618fb5c
juanvolpe <juan.volpe90@gmail.com>
2025-01-26 22:59:43 -0500

    Update database configuration and environment variables

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'productivityappmzcopy2copy',
+  location: 'us-central1'
+};
+
